aef073ec1259f252d65b8e02b967aa4dcb606d97
Lê Quang Lộc <lqloc112120@gmail.com>
2026-01-31 17:57:04 +0700

    get all orders

--- src/main/java/com/loc/electronics_store/repository/OrderRepository.java
+++ src/main/java/com/loc/electronics_store/repository/OrderRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
-    List<Order> findByUser(User user);
+    List<Order> findByUser_Username(String username);
 
     List<Order> findByStatus(String status);
 }
--- src/main/java/com/loc/electronics_store/service/OrderService.java
+++ src/main/java/com/loc/electronics_store/service/OrderService.java
@@ -1,5 +1,10 @@
 package com.loc.electronics_store.service;
 
+import com.loc.electronics_store.dto.response.order.OrderResponse;
+
+import java.util.List;
+
 public interface OrderService {
     String createOrder(String paymentMethod);
+    List<OrderResponse> getAll();
 }
